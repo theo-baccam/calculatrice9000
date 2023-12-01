8861fde73bebd80036aed37767b1fd5604145de3
theo-baccam <theo.baccam@laplateforme.io>
2023-12-01 16:59:12 +0100

    format black, fonctionalite historique très basique

--- calculatrice.py
+++ calculatrice.py
@@ -1,21 +1,28 @@
 import string
 
+current_history = []
+
+
 def input_processor(input_string):
     processed = [item for item in input_string.split(" ")]
     return processed
 
+
 def num_verify(num_1, num_2):
     if (
-        num_1 and num_1.replace(".", "", 1).isdigit() and
-        num_2 and num_2.replace(".", "", 1).isdigit()
+        num_1
+        and num_1.replace(".", "", 1).isdigit()
+        and num_2
+        and num_2.replace(".", "", 1).isdigit()
     ):
         return True
+
 
 def calculs(operator, num_1, num_2):
     ZeroDiv = "Division par zéro pas possible"
     operators = {
-        "**": num_1 ** num_2,
-        "*": num_1 * num_2, 
+        "**": num_1**num_2,
+        "*": num_1 * num_2,
         "/": num_1 / num_2 if num_2 != 0 else ZeroDiv,
         "//": num_1 // num_2 if num_2 != 0 else ZeroDiv,
         "%": num_1 % num_2 if num_2 != 0 else ZeroDiv,
@@ -28,17 +35,20 @@ def calculs(operator, num_1, num_2):
     else:
         return "Opérateur invalide"
 
+
+def history_append(num_1, operator, num_2, result, history_list):
+    history_list.append(f"{num_1} {operator} {num_2} = {result}")
+
+
 print("Calculatrice de Théo Baccam (tapez `help`): ")
 while True:
-
     try:
         prompt = input("==> ")
         processed = input_processor(prompt)
 
-        if processed[0] == "quit" or processed [0] == "exit":
+        if processed[0] == "quit" or processed[0] == "exit":
             print("Calculatrice fermée.")
             break
-
 
         if processed[0] == "help":
             print(
@@ -48,8 +58,14 @@ while True:
             )
             continue
 
+        if processed[0] == "hist":
+            for item in current_history:
+                print(item)
+            print("")
+            continue
+
         if len(processed) != 3:
-            print("Opération invalide")
+            print("Erreur: Ni une commande, ni une opération valide.\n")
             continue
 
         operator = processed[1]
@@ -61,6 +77,7 @@ while True:
             continue
 
         result = calculs(operator, float(num_1), float(num_2))
+        history_append(num_1, operator, num_2, result, current_history)
         print(f"{result}\n")
 
     except KeyboardInterrupt:
